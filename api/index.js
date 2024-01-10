5947b4d8cd0bfc8ab8314fa6f90df90b2891769d
rohit <rk4342244@gmail.com>
2024-01-10 22:38:54 +0530

    ok

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+// import { configDotenv } from "dotenv";
+// configDotenv();
+import dotenv from "dotenv"
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Successfully connected to atlas");
+}).catch((e)=>{
+    console.log(e);
+})
 let app=express();
 app.get("/",(req,res)=>{
     res.send(`ohhhhh ${req.query.name}`);
