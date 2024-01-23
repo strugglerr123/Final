976e1c82ad195623c5546f25caa9880a75adb643
rohit <rk4342244@gmail.com>
2024-01-23 23:55:47 +0530

    backend_Sign_in_completed

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcrypt";
+import { ErrorHandler } from "../utilis/error.js";
+import jwt from "jsonwebtoken";
 
 export let SignUp=async (req,res,next)=>{
     // console.log(req.body);
@@ -13,6 +15,21 @@ export let SignUp=async (req,res,next)=>{
         res.status(201).json(`user authentication successful !!!!`)
     } catch (error) {
         
+        next(error);
+    }
+}
+
+export let SignIn=async (req,res,next)=>{
+    let {email,passwd}=req.body;
+    try {
+        let validuser=await User.findOne({email:email});
+        if(!validuser)return next(ErrorHandler(404,"user not found"));
+        let validpasswd=bcryptjs.compareSync(passwd,validuser.passwd);
+        if(!validpasswd)return next(ErrorHandler(401,"Wrong Informations has been provided"));
+        let token=jwt.sign({id:validuser._id},process.env.JWT_SECRET);
+        let {passwd:pass,...rest}=validuser._doc;
+        res.cookie("Access_token",token,{httpOnly:true}).status(200).json(rest);
+    } catch (error) {
         next(error);
     }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { SignUp } from "../controllers/auth.controllers.js";
+import { SignUp,SignIn } from "../controllers/auth.controllers.js";
 let router = express.Router();
 
 router.post("/SignUp",SignUp);
+router.post("/Sign-in",SignIn);
 
 export default router
