17d6e7ade1612ce2651584b0d0d0e279e7485d0a
rohit <rk4342244@gmail.com>
2024-02-07 09:36:35 +0530

    Completed the ui of profile page

--- client/src/components/Private_profile.js
+++ client/src/components/Private_profile.js
@@ -3,7 +3,15 @@ import { useSelector } from "react-redux"
 import { Outlet, Navigate } from "react-router-dom"
 
 export let Private_profile = () => {
-  let { currentuser } = useSelector((state) => state.user)
-  //   console.log(currentuser.username)
-  return currentuser ? <Outlet /> : <Navigate to={"/sign-in"} />
+  let { currentuser } = useSelector((state) => state.user);
+  // let [bool,setbool]=React.useState(0);
+  // React.useEffect(()=>{
+  //   if(currentuser)setbool(1);
+  // },[])
+  return(
+  <>
+    {currentuser ? <Outlet/> : <Navigate to={"/sign-in"} />}
+    {/* {bool?<Profile data={currentuser.imageurl} />:null} */}
+  </>
+  )
 }
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,43 @@
 import React from "react"
+import { useSelector } from "react-redux"
+// import { Private_profile } from "../components/Private_profile"
 
 export let Profile = () => {
+  let { currentuser } = useSelector((state) => state.user)
   return (
-    <React.Fragment>
-      <h1>Welcome To Profile Page</h1>
-    </React.Fragment>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-center text-4xl '>
+        Welcome{" "}
+        <h1 className='inline text-green-500 underline'>
+          {currentuser.username}
+        </h1>
+      </h1>
+      <form action='profile' className='flex flex-col gap-3 mt-2'>
+        <img
+          src={currentuser.imageurl}
+          alt='Error in Loading'
+          className='h-32 w-32 rounded-[100px] cursor-pointer self-center'
+        />
+        <input
+          type='username'
+          placeholder='Username'
+          className='p-3 rounded-lg'
+        />
+        <input type='email' placeholder='email' className='p-3 rounded-lg' />
+        <input
+          type='passwd'
+          placeholder='password'
+          className='p-3 rounded-lg '
+        />
+        <button className='uppercase font-bold bg-slate-700 p-3 rounded-lg hover:opacity-90 text-white'>
+          Update
+        </button>
+      </form>
+
+      <form action='Query' className='flex justify-between mt-4'>
+        <span className='text-red-600  font-semibold'>Delete Account</span>
+        <span className='text-red-600 font-semibold'>Sign Out</span>
+      </form>
+    </div>
   )
 }
