b447783aae11c272275e8c70f73378963c2cd7d7
rohit <rk4342244@gmail.com>
2024-03-24 06:22:57 +0530

    final

--- client/src/pages/Listing.js
+++ client/src/pages/Listing.js
@@ -1,6 +1,63 @@
 import React from "react"
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage"
+import { app } from "../Firebase.js"
 
 export default function Listing() {
+  let [files, setfiles] = React.useState([])
+
+  let [formdata, setformdata] = React.useState({
+    imageurl: [],
+  })
+
+  console.log(files)
+
+  let Storefile = (e) => {
+    if (files.length > 0 && files.length <= 6) {
+      let allfiles = []
+      for (let i = 0; i < files.length; i++) {
+        allfiles.push(Upload_one_one(files[i]))
+      }
+
+      Promise.all(allfiles).then((urls) => {
+        setformdata({ ...formdata, imageurl: formdata.imageurl.concat(urls) })
+      })
+    }
+  }
+
+  let Upload_one_one = async (file) => {
+    return new Promise((resolve, reject) => {
+      let storage = getStorage(app)
+      let filename = new Date().getTime() + file.name
+      let storageref = ref(storage, filename)
+      let uploadfile = uploadBytesResumable(storageref, file)
+
+      uploadfile.on(
+        "state_changed",
+        (snapshot) => {
+          let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          console.log(`completed ${progress}`)
+        },
+
+        (error) => {
+          reject(error)
+        },
+
+        () => {
+          getDownloadURL(uploadfile.snapshot.ref).then((foundurl) => {
+            resolve(foundurl)
+          })
+        }
+      )
+    })
+  }
+
+  console.log(formdata)
+
   return (
     <main className=' p-2 max-w-4xl mx-auto'>
       <h1 className='my-4 flex flex-col uppercase font-semibold text-center text-4xl'>
@@ -107,8 +164,15 @@ export default function Listing() {
               accept='images/*'
               multiple
               className='P-2 border border-gray-600 rounded w-full self-center'
+              onClick={(e) => {
+                setfiles(e.target.files)
+              }}
             />
-            <button className='p-[1.5] text-red-400 border border-red-600 uppercase hover:shadow-lg disabled:opacity-70 rounded'>
+            <button
+              type='button'
+              className='p-[1.5] text-red-400 border border-red-600 uppercase hover:shadow-lg disabled:opacity-70 rounded'
+              onClick={Storefile}
+            >
               upload
             </button>
           </div>
